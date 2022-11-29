ed5a7648b0c7210047cdb22df88937307a9db830
Bryan Hickey <bjh@bjh.dev>
2022-11-29 13:32:52 +1100

    added vercel/analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "../layout";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
